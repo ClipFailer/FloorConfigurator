7e5acf7d6017e3c52bfb67a23e18e40377da0725
clipfail <rogroty@gmail.com>
2026-09-10 23:10:27 +0400

    Init

---
+++ Source/FloorConfigurator/FloorConfigurator.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FloorConfigurator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FloorConfigurator, "FloorConfigurator" );
